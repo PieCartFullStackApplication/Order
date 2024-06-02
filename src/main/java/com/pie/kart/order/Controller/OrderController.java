8c09c55ab280a964c9d2962a30c9c8f51fa166f1
yaswanthramanam <63838667+yaswanthramanam@users.noreply.github.com>
2024-06-02 07:01:57 +0530

    Cross Origin config 02-06-2024

---
+++ src/main/java/com/pie/kart/order/Controller/OrderController.java
@@ -0,0 +1,9 @@
+package com.pie.kart.order.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class OrderController {
+    
+}
